05489b0c1b97b37fd3b86e5fe0750afa61afd94f
clux <analsandblaster@gmail.com>
2015-12-31 23:48:09 +0100

    implement caching

--- bin.js
+++ bin.js
@@ -22,11 +22,22 @@ var cfgPath = require('confortable')('.yrcli.json', process.cwd());
 if (!cfgPath) {
   throw new Error('.yrcli.json not found');
 }
+var fs = require('fs');
 var cfg = require(cfgPath);
+if (!cfg.cacheFile) {
+  throw new Error('.yrcli.json must specify a cacheFile');
+}
+
+cfg.cacheFile = cfg.cacheFile.replace(/~|\$HOME/, process.env.HOME);
+if (!fs.existsSync(cfg.cacheFile)) {
+  fs.writeFileSync(cfg.cacheFile, '{}');
+}
+var cache = require(cfg.cacheFile);
 
 require('co')(function *() {
-  var data = yield yr(argv, cfg);
+  var data = yield yr(argv, cfg, cache);
   console.log(JSON.stringify(data));
+  fs.writeFileSync(cfg.cacheFile, JSON.stringify(cache, null, 2)); // update cache
 }).catch((err) => {
   console.error(err);
   process.exit(1);
